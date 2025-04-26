0e144dcf10547a2094029bfb4145785b1b8ab5e3
Aniket Chavan <anikettchavan103@gmail.com>
2025-04-26 14:49:34 +0530

    new feature added

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,16 +1,47 @@
 package com.example.demo;
 
 
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class DemoController {
 
+    List<String> teams = new ArrayList<>();
+    List<String> users = new ArrayList<>();
+
     @GetMapping("/health")
     public String checkSystem(){
 
-        return "ok";
+        return "System is Running...";
     }
+
+    @GetMapping("/teams/all")
+    public List<String> getteamsList(){
+        teams.add("Prices");
+        teams.add("Security");
+        teams.add("BAU");
+        teams.add("Automation");
+        teams.add("Deployment");
+
+        return teams;
+    }
+
+    @GetMapping("/teams/{teamName}")
+    public List<String> getTeamsUsers(@PathVariable String teamName){
+
+        users.add("harshad");
+        users.add("Omesh");
+        users.add("Aniket");
+
+        return users;
+
+    }
+
 
 }
